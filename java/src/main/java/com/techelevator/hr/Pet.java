158884a0155b7bd37975cad9aaba4163166bc5ed
Jeff Gould <gould.j88@gmail.com>
2021-01-29 15:11:03 -0500

    Pet Class

---
+++ java/src/main/java/com/techelevator/hr/Pet.java
@@ -0,0 +1,56 @@
+package com.techelevator.hr;
+
+import java.util.ArrayList;
+
+
+public class Pet {
+	
+	private String name;
+	private String species;
+	ArrayList<String> vaccinations = new ArrayList<String>();
+	
+	
+	public Pet(String name, String species) {
+		this.name = name;
+		this.species = species;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSpecies() {
+		return species;
+	}
+
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+
+	public ArrayList<String> getVaccinations() {
+		return vaccinations;
+	}
+
+
+	public void setVaccinations(ArrayList<String> vaccinations) {
+		this.vaccinations = vaccinations;
+	}
+	
+	public String listVaccinations() {
+		String shots = "";
+		for(int i = 0; i < vaccinations.size(); i++) 
+			shots = shots + vaccinations.get(i) + ", ";
+		
+		return shots;
+	}
+	
+}
